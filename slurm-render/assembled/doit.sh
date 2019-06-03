8772074eadf7b448d17d1de9ab0f6511a1f91698
Kartik Chandra <kach@rice11.stanford.edu>
2019-06-03 11:38:57 -0700

    added slurm-based distributed rendering paraphernalia

---
+++ slurm-render/assembled/doit.sh
@@ -0,0 +1,3 @@
+~/rocket-pbrt/rocket-pbrt/build/imgtool assemble --outfile tada.exr ../*.exr
+~/rocket-pbrt/rocket-pbrt/build/imgtool convert tada.exr tada.png
+mv tada.png ~/afs-home/WWW/tada.png
---
+++ slurm-render/launch.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+i=$1
+j=$2
+k=$3
+l=$4
+
+~/rocket-pbrt/rocket-pbrt/build/pbrt --outfile "out.$i.$k.exr" --cropwindow $i $j $k $l ../main.pbrt
