38727e4d48dfc371ba603606d3ac8857040894bc
Kartik Chandra <kach@rice11.stanford.edu>
2019-06-03 11:48:18 -0700

    upgraded the daemon

--- slurm-render/assembled/doit.sh
+++ slurm-render/assembled/doit.sh
@@ -1,3 +1,8 @@
-~/rocket-pbrt/rocket-pbrt/build/imgtool assemble --outfile tada.exr ../*.exr
-~/rocket-pbrt/rocket-pbrt/build/imgtool convert tada.exr tada.png
-mv tada.png ~/afs-home/WWW/tada.png
+#!/bin/bash
+while true; do
+	date;
+	~/rocket-pbrt/rocket-pbrt/build/imgtool assemble --outfile tada.exr ../*.exr;
+	~/rocket-pbrt/rocket-pbrt/build/imgtool convert tada.exr tada.png;
+	mv tada.png ~/afs-home/WWW/tada.png;
+	sleep 10;
+done
